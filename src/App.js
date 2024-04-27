e6196ded5619526c31a01c08599b29185b849e99
MattMorrell15 <morrel18@msu.edu>
2024-04-27 14:56:49 -0400

    final image and API touches

--- src/App.js
+++ src/App.js
@@ -28,22 +28,55 @@ function Library() {
 }
 
 
+function Librarytwo() {
+  const [myMixes, setMyMixes] = useState([]);
+  async function getMixes() {
+    let { data: Mixes, error } = await supabase
+      .from('Mixes')
+      .select('*');
+    setMyMixes(Mixes);
+  }
+  getMixes();
+  return (
+    <table>
+    {
+      myMixes.map(m => (
+        <tr>
+          <td>{m.title}</td>
+          <td>{m.artist}</td>
+        </tr>
+      ))
+    }
+    </table>
+  );
+}
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="lazersharkprofilepic.jpg" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Matt's Music Ground
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://scontent.fdet1-2.fna.fbcdn.net/v/t39.30808-6/404963199_713944270825735_1129650481302484129_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=_SxNo0agiB4Ab4pOBLu&_nc_oc=AdjaHsm01s8fgMOIYCqWy8Poa3I9grSgAnntVrdXwbkAgSkkk5iqC35wwU35aL2d0DU&_nc_ht=scontent.fdet1-2.fna&oh=00_AfBH2oRJTc3AxVEtV2w7u0_iUsxNDXoN0sodBSmV46rEQw&oe=6633255A"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Matt
+          <h1>Songs</h1>
+
           <Library />
+
+         <h1>Mixes</h1>
+          <Librarytwo />
+
         </a>
       </header>
     </div>
